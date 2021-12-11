ec779f0b1be3497c2ff3910b34400facc1c0ac92
MaartenPauwels <43341003+MaartenPauwels@users.noreply.github.com>
2021-12-11 12:56:01 +0100

    Update ex7.hs
    I think this is what they mean:
    two generators can be re-expressed using two comprehensions with single generators. Hint: nest one comprehension within the other and make use of the
    library function concat :: [[a]] -> [a].
    so i now have 2 list comprehensions with single generators

--- ch05/ex7.hs
+++ ch05/ex7.hs
@@ -1,4 +1,8 @@
 concat :: [[a]] -> [a]
 concat xss = [x | xs <- xss, x <- xs]
 
+
+
 result = Main.concat [[(x, y) | y <- [3, 4]] | x <- [1, 2]]
+
+--re a b = concat [  [(x,head [y | y <- b]),(x,([y | y <- b])!!1 ) ]| x <- a]
